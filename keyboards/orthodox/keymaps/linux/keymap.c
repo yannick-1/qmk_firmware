df50e82e31fadea22e6b4500b6e0200e92b712eb
untitled <y@noreply.github.com>
2020-06-06 10:50:32 +1200

    Orthodox linux layout has less layers; flashing now includes orthodox command.

--- keyboards/orthodox/keymaps/linux/keymap.c
+++ keyboards/orthodox/keymaps/linux/keymap.c
@@ -1,4 +1,3 @@
-    
 #include "orthodox.h"
 #include "action_layer.h"
 #include "eeconfig.h"
@@ -27,27 +26,21 @@ enum custom_keycodes {
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
     [_COLEMAK] = LAYOUT(\
-        KC_TAB,  KC_Q, KC_W, KC_F, KC_P, KC_G,                                                                                 KC_J, KC_L, KC_U,    KC_Y,   KC_SCLN,  KC_BSPC,  \
-        KC_BSPC, KC_A, KC_R, KC_S, KC_T, KC_D,               KC_LGUI,      KC_PGUP,     KC_PGDOWN, KC_LALT,                    KC_H, KC_N, KC_E,    KC_I,   KC_O,     KC_ENT,   \
-        KC_LSFT, KC_Z, KC_X, KC_C, KC_V, KC_B, LT(2,KC_SPC), LT(1,KC_SPC), KC_LCTL,     KC_RCTL,   LT(1,KC_DEL), LT(3,KC_DEL), KC_K, KC_M, KC_COMM, KC_DOT, KC_SLASH, KC_RSFT   \
+        KC_TAB,  KC_Q, KC_W, KC_F, KC_P, KC_G,                                                                            KC_J, KC_L, KC_U,    KC_Y,   KC_SCLN,  KC_BSPC,  \
+        KC_BSPC, KC_A, KC_R, KC_S, KC_T, KC_D,               KC_LGUI,      KC_PGUP,     KC_PGDOWN, KC_LALT,               KC_H, KC_N, KC_E,    KC_I,   KC_O,     KC_ENT,   \
+        KC_LSFT, KC_Z, KC_X, KC_C, KC_V, KC_B, LT(1,KC_SPC), LT(1,KC_SPC), KC_LCTL,     KC_RCTL,   LT(2,KC_DEL), KC_LALT, KC_K, KC_M, KC_COMM, KC_DOT, KC_SLASH, KC_RSFT   \
     ),
 
     [_LAYER_1] = LAYOUT(\
-        KC_GESC, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC,                                                           KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, _______,  \
-        _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,            _______, _______,     _______, _______,          KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,   \
-        _______, _______, _______, _______, _______, _______, _______, _______, _______,     _______, _______, _______, _______, _______, _______, _______, _______, KC_F12    \
+        KC_GESC, _______, _______, KC_LPRN, KC_RPRN, _______,                                                             KC_CIRC, KC_LABK, KC_UP,   KC_RABK,  KC_AMPR,  KC_BSLS,  \
+        _______, _______, _______, KC_LBRC, KC_RBRC, KC_PGUP,            _______, _______,     _______, _______,          KC_PERC, KC_LEFT, KC_DOWN, KC_RIGHT, KC_QUOT,  KC_EXLM,  \
+        _______, _______, _______, KC_LCBR, KC_RCBR, KC_PGDOWN, _______, _______, RESET,       _______, _______, _______, KC_EQL,  KC_PLUS, KC_MINS, KC_ASTR,  KC_SLASH, KC_UNDS   \
     ),
 
     [_LAYER_2] = LAYOUT(\
-        KC_GESC, _______, _______, KC_LPRN, KC_RPRN, _______,                                                           KC_CIRC, KC_LABK, KC_UP,   KC_RABK,  KC_AMPR,  KC_BSLS,  \
-        _______, _______, _______, KC_LBRC, KC_RBRC, _______,          _______, _______,     _______, _______,          KC_PERC, KC_LEFT, KC_DOWN, KC_RIGHT, KC_QUOT,  KC_EXLM,  \
-        _______, _______, _______, KC_LCBR, KC_RCBR, _______, _______, _______, RESET,       _______, _______, _______, KC_EQL,  KC_PLUS, KC_MINS, KC_ASTR,  KC_SLASH, KC_UNDS   \
-    ),
-
-    [_LAYER_3] = LAYOUT(\
-        KC_GESC, _______, KC_MUTE, KC_VOLD, KC_VOLU, _______,                                                           _______,  KC_7, KC_8, KC_9, _______, _______,  \
-        _______, KC_MPRV, KC_MPLY, KC_MNXT, _______, _______,          _______, KC_PSCR,     _______, _______,          _______,  KC_4, KC_5, KC_6, _______, _______,  \
-        _______, _______, _______, _______, _______, _______, _______, _______, _______,     _______, _______, _______, KC_0,     KC_1, KC_2, KC_3, _______, _______   \
+        KC_GESC, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC,                                                           _______,  KC_7, KC_8, KC_9, KC_VOLU, KC_MNXT,  \
+        _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,            KC_F11,  KC_F12,      _______, _______,          _______,  KC_4, KC_5, KC_6, KC_VOLD, KC_MPLY,  \
+        _______, KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  _______, _______, _______,     _______, _______, _______, KC_0,     KC_1, KC_2, KC_3, KC_MUTE, KC_MPRV   \
     ),
 
     [_ADJUST] =  LAYOUT(\
@@ -122,3 +115,4 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record)
     }
     return true;
 }
+
