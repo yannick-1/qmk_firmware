fafced0af372297ea348e0788e0870af78dee27a
y <y@noreply.github.com>
2020-06-09 14:42:30 +1200

    .

--- keyboards/orthodox/keymaps/linux/keymap.c
+++ keyboards/orthodox/keymaps/linux/keymap.c
@@ -36,9 +36,9 @@ qk_tap_dance_action_t tap_dance_actions[] = {
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
     [_COLEMAK] = LAYOUT(\
-        TD(TD_TAB_GESC),  KC_Q, KC_W, KC_E, KC_R, KC_T,                                                                            KC_Y, KC_U, KC_I,    KC_O,   KC_P,  KC_BSPC,  \
-        KC_BSPC, KC_A, KC_S, KC_D, KC_F, KC_G,               KC_LGUI,      KC_PGUP,     KC_PGDOWN, KC_LALT,               KC_H, KC_J, KC_K,    KC_L,   KC_SCLN,     KC_ENT,   \
-        KC_LSFT, KC_Z, KC_X, KC_C, KC_V, KC_B, LT(1,KC_SPC), LT(1,KC_SPC), KC_LCTL,     KC_RCTL,   LT(2,KC_DEL), KC_LALT, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLASH, KC_RSFT   \
+        TD(TD_TAB_GESC), KC_Q, KC_W, KC_E, KC_R, KC_T,                                                                            KC_Y, KC_U, KC_I,    KC_O,   KC_P,     KC_BSPC,  \
+        KC_BSPC,         KC_A, KC_S, KC_D, KC_F, KC_G,               KC_LGUI,      KC_PGUP,     KC_PGDOWN, KC_LALT,               KC_H, KC_J, KC_K,    KC_L,   KC_SCLN,  KC_ENT,   \
+        KC_LSFT,         KC_Z, KC_X, KC_C, KC_V, KC_B, LT(1,KC_SPC), LT(1,KC_SPC), KC_LCTL,     KC_RCTL,   LT(2,KC_DEL), KC_LALT, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLASH, KC_RSFT   \
     ),
 
     [_LAYER_1] = LAYOUT(\
